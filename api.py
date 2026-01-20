0b3ddcd06e4656aa6d2bf7ce5a5d8d1bc752c9b0
Joljolionn <joljolionn@gmail.com>
2026-01-20 14:13:43 -0300

    fix: arrumando rota de delete para mobile

--- api.py
+++ api.py
@@ -29,8 +29,17 @@ def add_new_item(new_item_title: db.ItemTitleRequestDto):
 def delete_item(id: db.ItemIdRequestDto):
     rows_affected: int
     with db._Session() as session:
+<<<<<<< HEAD
         rows_affected = session.query(db.Item).filter(db.Item.id == id.id).delete()
     return rows_affected == 1
+=======
+        query = session.query(db.Item).filter(db.Item.id == id.id)
+        print(f"Tentando deletar item com ID: {id.id}")  # Veja se o ID bate
+        rows_affected = query.delete()
+        session.commit()
+        print(f"Linhas afetadas: {rows_affected}")
+    return {"success": rows_affected > 0, "rows": rows_affected}
+>>>>>>> 4bcdf90 (fix: arrumando rota de delete para mobile)
 
 
 @mobileRouter.patch("/increase")
@@ -42,15 +51,27 @@ def increase_item_num(id: db.ItemIdRequestDto):
         session.refresh(item)
     return item
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 4bcdf90 (fix: arrumando rota de delete para mobile)
 @mobileRouter.patch("/decrease")
 def decrease_item_num(id: db.ItemIdRequestDto):
     with db._Session() as session:
         item = session.query(db.Item).filter(db.Item.id == id.id).first()
+<<<<<<< HEAD
         item.num = item.num-1 if item.num > 1 else item.num
+=======
+        item.num = item.num - 1 if item.num > 1 else item.num
+>>>>>>> 4bcdf90 (fix: arrumando rota de delete para mobile)
         session.commit()
         session.refresh(item)
     return item
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 4bcdf90 (fix: arrumando rota de delete para mobile)
 @mobileRouter.patch("/completed")
 def toggle_completed_item(id: db.ItemIdCompletedRequestDto):
     with db._Session() as session:
@@ -58,4 +79,7 @@ def toggle_completed_item(id: db.ItemIdCompletedRequestDto):
         item.completed = id.completed
         session.commit()
         session.refresh(item)
+<<<<<<< HEAD
     
+=======
+>>>>>>> 4bcdf90 (fix: arrumando rota de delete para mobile)
